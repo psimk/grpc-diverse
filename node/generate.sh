a2b4993637483453f7307d25bbc9668e90db11b2
Paul Simk <paulius.simkevicius@mediamonks.com>
2018-12-13 20:50:08 +0100

    -webgRPC service stubs

--- node/generate.sh
+++ node/generate.sh
@@ -1,7 +1,6 @@
 # #!/bin/sh
 PROTOC_PLUGIN_PATH="node_modules/.bin/grpc_tools_node_protoc_plugin"
 PROTOC_PLUGIN_GRPC_TS_PATH="node_modules/grpc_tools_node_protoc_ts/bin/protoc-gen-ts"
-PROTOC_PLUGIN_TS_PATH="node_modules/.bin/protoc-gen-ts"
 
 OUT_DIR="generated"
 
@@ -26,12 +25,5 @@ protoc \
 protoc \
   --plugin="protoc-gen-grpc=${PROTOC_PLUGIN_GRPC_TS_PATH}" \
   --grpc_out="${OUT_DIR}" \
-  -I=../proto \
-  ../proto/*.proto
-
-# Generate Web GRPC service stubs
-protoc \
-  --plugin="protoc-gen-ts=${PROTOC_PLUGIN_TS_PATH}" \
-  --ts_out="service=true:${OUT_DIR}" \
   -I=../proto \
   ../proto/*.proto
